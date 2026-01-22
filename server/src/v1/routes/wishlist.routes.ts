af9da365f3c0d5ec5356427a4506d542a8921cc8
illiad24 <drapakilla2@.com>
2026-01-22 22:41:10 +0200

    space

--- server/src/v1/routes/wishlist.routes.ts
+++ server/src/v1/routes/wishlist.routes.ts
@@ -2,7 +2,8 @@ import { Router } from "express";
 const router = Router();
 import { WishlistController } from "../controllers/wishlist.controller";
 import { authMiddleware as auth } from "../../middlewares/auth";
-router.get("/", auth, WishlistController.get);
 
+router.get("/", auth, WishlistController.get);
 router.post("/:productId/toggle", auth, WishlistController.toggle);
+
 export default router;
