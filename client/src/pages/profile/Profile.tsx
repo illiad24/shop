d5541c7fd3c921a0fbe01081db1e28403f129cc7
illiad24 <drapakilla2@gmail.com>
2026-02-22 22:00:10 +0200

    profile

--- client/src/pages/profile/Profile.tsx
+++ client/src/pages/profile/Profile.tsx
@@ -1,8 +1,38 @@
+
+import { Icon } from "@/shared/icons/Icon";
+import { Outlet } from "react-router";
+
 export function Profile() {
     return (
-        <div>
-            <div className="container">
-
+        <div className="pt-40">
+            <div className="container grid grid-cols-[320px_1fr]">
+                <div className="bg-white rounded-[12px] p-5">
+                    <div>
+                        user info
+                    </div>
+                    <div>
+                        <div className="flex gap-2 items-center px-5 py-2.5 mb-4 last:mb-0">
+                            <div>
+                                <Icon name="favorite" className="text-[14px] font-light" />
+                            </div>
+                            <div className=" font-light text-[18px]">
+                                Вподобані  товари
+                            </div>
+                        </div>
+                        <div className="flex gap-2 items-center px-5 py-2.5 mb-4 last:mb-0">
+                            <div>
+                                <Icon name="favorite" className="text-[14px] font-light" />
+                            </div>
+                            <div className="font-light text-[18px]">
+                                Вподобані  товари
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div>
+                    content
+                    <Outlet />
+                </div>
             </div>
         </div>
     )
