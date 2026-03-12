bddfd2886a1355723305f662ead57a2336289f3f
illiad24 <drapakilla2@gmail.com>
2026-03-12 20:02:24 +0200

    update mobile version MainSection

--- client/src/widgets/sections/MainSection.tsx
+++ client/src/widgets/sections/MainSection.tsx
@@ -16,50 +16,50 @@ export function MainSection() {
                 effect={'fade'}
                 speed={1200}
             >
-                <SwiperSlide className="relative min-h-[100vh] flex items-center content-center pt-20">
+                <SwiperSlide className="relative min-h-svh flex items-center content-center pt-20">
                     <img className="absolute-element object-cover" src="/homepage/main/01.png" alt="Свіжі морепродукти" />
-                    <div className="container relative z-40 h-full " >
+                    <div className="container relative z-40 h-full">
                         <div>
                             <div className="max-w-[700px]">
-                                <h1 className="font-bold text-[64px] text-main mb-8 last:mb-0">Смакуйте свіжість океану</h1>
-                                <div className="text-main text-[18px] mb-10 last:mb-0">
+                                <h1 className="font-bold text-[36px] sm:text-[48px] lg:text-[64px] text-main mb-4 sm:mb-8 last:mb-0">Смакуйте свіжість океану</h1>
+                                <div className="text-main text-[16px] sm:text-[18px] mb-6 sm:mb-10 last:mb-0">
                                     <p>
                                         Відкрийте для себе неперевершений смак свіжих морепродуктів, доставлених прямо до вашого столу.
                                     </p>
                                 </div>
-                                <a href="/products" className="button-el">Переглянути асортимент</a>
+                                <a href="/products" className="text-[16px] sm:text-[20px] inline-block text-white bg-orange-1 rounded-xl px-5 py-3 hover:bg-orange-1/50 transition-all">Переглянути асортимент</a>
                             </div>
                         </div>
                     </div>
                 </SwiperSlide>
-                <SwiperSlide className="relative min-h-[100vh] flex items-center content-center pt-20">
+                <SwiperSlide className="relative min-h-svh flex items-center content-center pt-20">
                     <img className="absolute-element object-cover" src="/homepage/main/02.png" alt="Вишукані делікатеси" />
-                    <div className="container relative z-40 h-full " >
+                    <div className="container relative z-40 h-full">
                         <div>
                             <div className="max-w-[700px]">
-                                <h1 className="font-bold text-[64px] text-main mb-8 last:mb-0">Вишукані делікатеси для гурманів</h1>
-                                <div className="text-main text-[18px] mb-10 last:mb-0">
+                                <h1 className="font-bold text-[36px] sm:text-[48px] lg:text-[64px] text-main mb-4 sm:mb-8 last:mb-0">Вишукані делікатеси для гурманів</h1>
+                                <div className="text-main text-[16px] sm:text-[18px] mb-6 sm:mb-10 last:mb-0">
                                     <p>
                                         Зануртесь у світ неперевершених смаків з нашою колекцією відбірних морських делікатесів.
                                     </p>
                                 </div>
-                                <a href="/delicacies" className="button-el">Відкрити для себе</a>
+                                <a href="/delicacies" className="text-[16px] sm:text-[20px] inline-block text-white bg-orange-1 rounded-xl px-5 py-3 hover:bg-orange-1/50 transition-all">Відкрити для себе</a>
                             </div>
                         </div>
                     </div>
                 </SwiperSlide>
-                <SwiperSlide className="relative min-h-[100vh] flex items-center content-center pt-20">
-                    <img className="absolute-element" src="/homepage/main/01.png" alt="Кулінарна досконалість" />
-                    <div className="container relative z-40 h-full" >
+                <SwiperSlide className="relative min-h-svh flex items-center content-center pt-20">
+                    <img className="absolute-element object-cover" src="/homepage/main/01.png" alt="Кулінарна досконалість" />
+                    <div className="container relative z-40 h-full">
                         <div>
                             <div className="max-w-[700px]">
-                                <h1 className="font-bold text-[64px] text-main mb-8 last:mb-0">Кулінарна досконалість на вашому столі</h1>
-                                <div className="text-main text-[18px] mb-10 last:mb-0">
+                                <h1 className="font-bold text-[36px] sm:text-[48px] lg:text-[64px] text-main mb-4 sm:mb-8 last:mb-0">Кулінарна досконалість на вашому столі</h1>
+                                <div className="text-main text-[16px] sm:text-[18px] mb-6 sm:mb-10 last:mb-0">
                                     <p>
                                         Створіть незабутні страви з нашими преміальними морепродуктами, які надихають на кулінарні шедеври.
                                     </p>
                                 </div>
-                                <a href="/recipes" className="button-el">Знайти рецепти</a>
+                                <a href="/recipes" className="text-[16px] sm:text-[20px] inline-block text-white bg-orange-1 rounded-xl px-5 py-3 hover:bg-orange-1/50 transition-all">Знайти рецепти</a>
                             </div>
                         </div>
                     </div>
